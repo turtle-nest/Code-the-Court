bbb3d55ef9fdd6a88362a2341e51c5f59c889f8d
turtle-nest <n78693107@gmail.com>
2025-06-28 14:53:23 +0200

    test(notes): add POST /api/notes Jest test and Postman request with response checks

--- routes/notes.js
+++ routes/notes.js
@@ -2,20 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
+const { createNote } = require('../controllers/notesController');
 
-router.post('/', authMiddleware, (req, res) => {
-  const user_id = req.user.id;
-  const { target_id, target_type, content } = req.body;
-
-  res.json({
-    message: 'Note created (fake)',
-    note: {
-      user_id,
-      target_id,
-      target_type,
-      content
-    }
-  });
-});
+router.post('/', authMiddleware, createNote);
 
 module.exports = router;
